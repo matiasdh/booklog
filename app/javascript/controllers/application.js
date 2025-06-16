6b6833a7d69f4789265f356bb16ad8a019b52c47
Matías de Horta <matias.dh@gmail.com>
2025-06-16 19:42:17 -0300

    Remove importmaps

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,13 +1,9 @@
 import { Application } from "@hotwired/stimulus"
-import {InputValidator} from "stimulus-inline-input-validations"
-
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
-
-application.register("input-validator", InputValidator)
 
 export { application }
