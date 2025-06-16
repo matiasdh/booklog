898d933e4e8bbe942243f4bff8aa49ff2ce08f55
Matías de Horta <matias.dh@gmail.com>
2025-06-16 13:30:44 -0300

    Create modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -1,25 +1,17 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["usernameInput"]
 
-  closeModal() {
-    window.controller = this
-    document.body.classList.remove("overflow-y-hidden")
-    window.history.back()
-    this.element.remove()
+  connect () {
+    document.body.classList.add("overflow-y-hidden")
   }
 
-  checkUsername() {
-    console.log("entra")
-    console.log(this.usernameInputTarget.value);
+  disconnect () {
+    document.body.classList.remove("overflow-y-hidden")
+  }
 
-    const username = this.usernameInputTarget.value.trim();
-    if (username.length < 3) {
-      this.usernameInputTarget.setCustomValidity("Username must be at least 3 characters long.");
-    } else {
-      this.usernameInputTarget.setCustomValidity("");
-    }
-    this.usernameInputTarget.reportValidity();
+  closeModal() {
+    window.history.back()
+    this.element.remove()
   }
 }
