e0e600f0bf06a6209bf658a7138b73c79ab04a2c
Matías de Horta <matias.dh@gmail.com>
2025-06-19 13:20:00 -0300

    Create user model

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :email, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :username, length: { minimum: 3 }
+end
