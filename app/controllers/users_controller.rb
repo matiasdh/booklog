8a4579fa988fb96aa2ec04bb3dbfbc9cb6bd2a74
Matías de Horta <matias.dh@gmail.com>
2025-06-22 08:00:00 -0300

    Add users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
+  def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
+  end
+
+  def user
+    @user ||= User.find(params[:id])
+  end
+  helper_method :user
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,10 +1,28 @@
+# spec/requests/posts_spec.rb
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET users#index" do
-    xit "returns http success" do
-      get "/"
-      expect(response).to have_http_status(:success)
+  let(:user) { create(:user) }
+
+  describe "GET /users/:user_id" do
+    context "when unauthenticated" do
+      it "redirects to login" do
+        get user_path(user)
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(new_user_session_path)
+      end
     end
+
+   context "when authenticated" do
+    before do
+      sign_in user
+    end
+
+    it "renders index for authenticated users" do
+      get user_path(user)
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(user.username)
+    end
+   end
   end
 end
